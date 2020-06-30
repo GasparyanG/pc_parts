019f689d0d7581dfc3404e0c4b0b63d8313a7987
Garnik Gasparyan <gasp.garnik@gmail.com>
2020-06-30 20:31:37 +0400

    Add relationships

--- src/Services/API/JsonApi/GPUHandler.php
+++ src/Services/API/JsonApi/GPUHandler.php
@@ -4,9 +4,15 @@
 namespace App\Services\API\JsonApi;
 
 
+use App\Database\Entities\Color;
+use App\Database\Entities\ExternalPowerType;
+use App\Database\Entities\FrameSyncType;
+use App\Database\Entities\GpuCoolingType;
 use App\Database\Entities\GpuImage;
 use App\Database\Entities\GpuPartNumber;
+use App\Database\Entities\GpuPort;
 use App\Database\Entities\GpuPrice;
+use App\Database\Entities\SliCrossfireType;
 use App\Database\Entities\VideoCard;
 
 class GPUHandler extends ResourceHandler
@@ -22,7 +28,13 @@ class GPUHandler extends ResourceHandler
     protected static $relationshipProperties = [
         GpuImage::class => "getGpuImages",
         GpuPrice::class => "getGpuPrices",
-        GpuPartNumber::class => "getPartNumbers"
+        GpuPartNumber::class => "getPartNumbers",
+        SliCrossfireType::class => "getSliCrossfireTypes",
+        FrameSyncType::class => "getFrameSyncType",
+        ExternalPowerType::class => "getExternalPowerTypes",
+        GpuCoolingType::class => "getGpuCoolingTypes",
+        GpuPort::class => "getGpuPorts",
+        Color::class => "getColors"
     ];
 
     public function included(?string $relToInclude, int $id): array
