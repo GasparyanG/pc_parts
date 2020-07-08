abaf69daf503a97a91f31874348665017cb2183f
Garnik Gasparyan <gasp.garnik@gmail.com>
2020-07-08 15:04:58 +0400

    Send response based on api key

--- src/Controllers/AbstractController.php
+++ src/Controllers/AbstractController.php
@@ -5,11 +5,14 @@ namespace App\Controllers;
 
 
 use App\Services\API\JsonApi\Specification\Response as JsonResponse;
+use App\Services\TemplateEngine\Twig\Twig;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 abstract class AbstractController
 {
+    const API = "api";
+
     /**
      * @var string|null
      */
@@ -30,9 +33,15 @@ abstract class AbstractController
 
     public function getCollection(Request $req, array $placeholders): Response
     {
-        $composer = new static::$composer(new static::$handler(), $req->query);
-        $composer->assembleCollection();
+        // response with api or not
+        $api = $req->query->get(self::API);
+        if ((bool)$api !== true) {
+            return Response::create((new Twig())->render("collection_of_part.html.twig"));
+        } else {
+            $composer = new static::$composer(new static::$handler(), $req->query);
+            $composer->assembleCollection();
 
-        return JsonResponse::success($composer->getResource());
+            return JsonResponse::success($composer->getResource());
+        }
     }
 }
