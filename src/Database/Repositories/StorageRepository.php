b9fff54db231d204b6a3bb0516a1069193737663
Garnik Gasparyan <gasp.garnik@gmail.com>
2020-07-22 14:36:45 +0400

    Add storage filters

--- src/Database/Repositories/StorageRepository.php
+++ src/Database/Repositories/StorageRepository.php
@@ -11,4 +11,65 @@ class StorageRepository extends EntityRepository
 {
     const TABLE_NAME = "storages";
     use RepositoryTrait;
+
+    public function findTypes()
+    {
+        $res = $this->createQueryBuilder('a')
+            ->select('st.id, st.type as name')
+            ->leftJoin("App\Database\Entities\StorageType", 'st', 'WITH', 'st=a.storageType')
+            ->groupBy('st.id')
+            ->getQuery()
+            ->getArrayResult();
+
+        if ($res) return $res;
+        return [];
+    }
+
+    public function findFormFactors()
+    {
+        $res = $this->createQueryBuilder('a')
+            ->select('sff.id, sff.type as name')
+            ->leftJoin("App\Database\Entities\StorageFormFactor", 'sff', 'WITH', 'sff=a.storageFormFactor')
+            ->groupBy('sff.id')
+            ->getQuery()
+            ->getArrayResult();
+
+        if ($res) return $res;
+        return [];
+    }
+
+    public function findInterfaces()
+    {
+        $res = $this->createQueryBuilder('a')
+            ->select('si.id, si.type as name')
+            ->leftJoin("App\Database\Entities\StorageInterface", 'si', 'WITH', 'si=a.storageInterface')
+            ->groupBy('si.id')
+            ->getQuery()
+            ->getArrayResult();
+
+        if ($res) return $res;
+        return [];
+    }
+
+    public function findCapacityMinAndMax()
+    {
+        $res = $this->createQueryBuilder('a')
+            ->select("MIN(a.capacity) as min, MAX(a.capacity) as max")
+            ->getQuery()
+            ->getArrayResult();
+
+        if ($res) return $res[0];
+        return [];
+    }
+
+    public function findCacheMinAndMax()
+    {
+        $res = $this->createQueryBuilder('a')
+            ->select("MIN(a.cache) as min, MAX(a.cache) as max")
+            ->getQuery()
+            ->getArrayResult();
+
+        if ($res) return $res[0];
+        return [];
+    }
 }
--- src/Services/API/JsonApi/StorageHandler.php
+++ src/Services/API/JsonApi/StorageHandler.php
@@ -12,6 +12,8 @@ use App\Database\Entities\StorageInterface;
 use App\Database\Entities\StoragePartNumber;
 use App\Database\Entities\StoragePrice;
 use App\Database\Entities\StorageType;
+use App\Services\API\JsonApi\DataFetching\FilterImplementer;
+use App\Services\API\JsonApi\Specification\Metadata;
 
 class StorageHandler extends ResourceHandler
 {
@@ -58,4 +60,86 @@ class StorageHandler extends ResourceHandler
         StorageFormFactor::class => "getStorageFormFactor",
         StorageInterface::class => "getStorageInterface"
     ];
+
+    protected function filtrationData(Metadata $meta): void
+    {
+        parent::filtrationData($meta);
+        $this->typeFIlter($meta);
+        $this->capacityFilter($meta);
+        $this->cacheFilter($meta);
+        $this->formFactorFilter($meta);
+        $this->interfaceFilter($meta);
+    }
+
+    protected function typeFilter(Metadata $meta): void
+    {
+        $types = $this->repo->findtypes();
+
+        $meta->addFiltrationData([
+            Metadata::COLLECTION => $types,
+            Metadata::TYPE => Metadata::CHECKBOX,
+            Metadata::GROUPING => Metadata::CHECKBOX_GROUPING,
+            Metadata::NAME => "Type",
+            Metadata::FIELD => "storageType",
+            Metadata::OPERATOR => strtolower(FilterImplementer::IN)
+        ]);
+    }
+
+    protected function capacityFilter(Metadata $meta): void
+    {
+        $capacityMinAndMax = $this->repo->findCapacityMinAndMax();
+
+        $meta->addFiltrationData([
+            Metadata::MIN => $capacityMinAndMax[Metadata::MIN] ?? 0,
+            Metadata::MAX => $capacityMinAndMax[Metadata::MAX] ?? 0,
+            Metadata::TYPE => Metadata::RANGE,
+            Metadata::GROUPING => Metadata::RANGE_GROUPING,
+            Metadata::NAME => "Capacity",
+            Metadata::FIELD => "capacity",
+            Metadata::OPERATOR => strtolower(FilterImplementer::BETWEEN)
+        ]);
+    }
+
+    protected function cacheFilter(Metadata $meta): void
+    {
+        $cacheMinAndMax = $this->repo->findCacheMinAndMax();
+
+        $meta->addFiltrationData([
+            Metadata::MIN => $cacheMinAndMax[Metadata::MIN] ?? 0,
+            Metadata::MAX => $cacheMinAndMax[Metadata::MAX] ?? 0,
+            Metadata::TYPE => Metadata::RANGE,
+            Metadata::GROUPING => Metadata::RANGE_GROUPING,
+            Metadata::NAME => "Cache",
+            Metadata::FIELD => "cache",
+            Metadata::OPERATOR => strtolower(FilterImplementer::BETWEEN)
+        ]);
+    }
+
+    protected function formFactorFilter(Metadata $meta): void
+    {
+        $formFactors = $this->repo->findFormFactors();
+
+        $meta->addFiltrationData([
+            Metadata::COLLECTION => $formFactors,
+            Metadata::TYPE => Metadata::CHECKBOX,
+            Metadata::GROUPING => Metadata::CHECKBOX_GROUPING,
+            Metadata::NAME => "Form Factor",
+            Metadata::FIELD => "storageFormFactor",
+            Metadata::OPERATOR => strtolower(FilterImplementer::IN)
+        ]);
+    }
+
+    protected function interfaceFilter(Metadata $meta): void
+    {
+        $interfaces = $this->repo->findInterfaces();
+
+        $meta->addFiltrationData([
+            Metadata::COLLECTION => $interfaces,
+            Metadata::TYPE => Metadata::CHECKBOX,
+            Metadata::GROUPING => Metadata::CHECKBOX_GROUPING,
+            Metadata::NAME => "Interface",
+            Metadata::FIELD => "storageInterface",
+            Metadata::OPERATOR => strtolower(FilterImplementer::IN)
+        ]);
+    }
 }
