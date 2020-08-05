e2dc3de6f89c5e81c508f54f83b680174ac19985
Garnik Gasparyan <gasp.garnik@gmail.com>
2020-08-05 20:12:11 +0400

    Do Further configuration and refactoring

--- public/js/collection_of_part/PartFiltration.js
+++ public/js/collection_of_part/PartFiltration.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Filtration } from "./Resource"
-import { showMore, hideFilter } from "./collection_helper_functions"
+import { showMore, hideFilter, composeClassName, hideFilterClassNames, showLessOrMoreClassNames } from "./collection_helper_functions"
 
 class PartFiltration extends React.Component {
     constructor(props) {
@@ -48,25 +48,40 @@ class CheckboxFilter extends React.Component {
         });
 
         let lessOrMore = (<div></div>);
-        let shrink = "";
+        let ShrinkClassName = "";
         if (checkboxes.length > 5) {
-            lessOrMore = (<div className={"show-more show-less-or-more show_" + this.props.filter.field} onClick={() => showMore(this.props.filter.field)}>&darr; show more</div>)
-            shrink = "filter-shrink";
+            lessOrMore =
+                (<div className={composeClassName(
+                    showLessOrMoreClassNames.show_more_class_name,
+                    showLessOrMoreClassNames.show_less_or_more_class_name,
+                    "show_" + this.props.filter.field)
+                }
+                      onClick={() => showMore(this.props.filter.field)}>&darr; show more</div>);
+            ShrinkClassName = showLessOrMoreClassNames.filter_shrink_class_name;
         }
 
         return (
             <div>
                 <div className="filter-header">
-                    <div className="filter-header-component filter-name">{this.props.filter.name}</div>
-                    <div onClick={() => hideFilter(this.props.filter.field)} className="filter-header-component hide-sign">
-                        <div className="rotating-sign">
-                            <div className={"sign-bar vertical-bar vertical_" + this.props.filter.field}></div>
-                            <div className={"sign-bar horizontal-bar horizontal_" + this.props.filter.field}></div>
+                    <div className="filter-name">{this.props.filter.name}</div>
+                    <div onClick={() => hideFilter(this.props.filter.field)} className={hideFilterClassNames.hide_sign_class_name}>
+                        <div className={hideFilterClassNames.rotating_sign_class_name}>
+                            <div className={composeClassName(
+                                hideFilterClassNames.sign_bar_class_name,
+                                hideFilterClassNames.vertical_bar_class_name,
+                                "vertical_" + this.props.filter.field)}></div>
+                            <div className={composeClassName(
+                                hideFilterClassNames.sign_bar_class_name,
+                                hideFilterClassNames.horizontal_bar_class_name,
+                                "horizontal_" + this.props.filter.field)}></div>
                         </div>
                     </div>
                 </div>
-                <div className={"product-filter filter-show filter_" + this.props.filter.field}>
-                    <div className={shrink}>
+                <div className={composeClassName(
+                    "product-filter",
+                    hideFilterClassNames.filter_show_class_name,
+                    "filter_" + this.props.filter.field)}>
+                    <div className={ShrinkClassName}>
                         {checkboxes}
                     </div>
                     <div>
@@ -95,14 +110,23 @@ class RangeFilter extends React.Component {
             <div>
                 <div className="filter-header">
                     <div className="filter-name">{this.props.filter.name}</div>
-                    <div className="hide-sign" onClick={() => hideFilter(this.props.filter.field)}>
-                        <div className="rotating-sign">
-                            <div className={"sign-bar vertical-bar vertical_" + this.props.filter.field}></div>
-                            <div className={"sign-bar horizontal-bar horizontal_" + this.props.filter.field}></div>
+                    <div className={hideFilterClassNames.hide_sign_class_name} onClick={() => hideFilter(this.props.filter.field)}>
+                        <div className={hideFilterClassNames.rotating_sign_class_name}>
+                            <div className={composeClassName(
+                                hideFilterClassNames.sign_bar_class_name,
+                                hideFilterClassNames.vertical_bar_class_name,
+                                "vertical_" + this.props.filter.field)}></div>
+                            <div className={composeClassName(
+                                hideFilterClassNames.sign_bar_class_name,
+                                hideFilterClassNames.horizontal_bar_class_name,
+                                "horizontal_" + this.props.filter.field)}></div>
                         </div>
                     </div>
                 </div>
-                <div className={"product-filter filter-show filter_" + this.props.filter.field}>
+                <div className={composeClassName(
+                    "product-filter",
+                    hideFilterClassNames.filter_show_class_name,
+                    "filter_" + this.props.filter.field)}>
                     <label htmlFor={this.props.filter.field + "_min" }>Min</label>
                     <input onChange={() => this.filter(this.props.filter)} id={this.props.filter.field + "_min" }
                            type="text" defaultValue={this.props.filter.min}/>
--- public/js/collection_of_part/collection_helper_functions.js
+++ public/js/collection_of_part/collection_helper_functions.js
@@ -1,3 +1,47 @@
+// Hide filter class names
+const filterCollapseClassNames = "filter-collapse";
+const signOpenClassName = "sign-open";
+const signCollapseClassName = "sign-collapse";
+const filterShowClassName = "filter-show";
+const signBarClassName = "sign-bar";
+const verticalBarClassName = "vertical-bar";
+const horizontalBarClassName = "horizontal-bar";
+const rotatingSignClassName = "rotating-sign";
+const hideSignClassName = "hide-sign";
+
+const hideFilterClassNames = {
+    filter_collapse_class_names: filterCollapseClassNames,
+    sign_open_class_name: signOpenClassName,
+    sign_collapse_class_name: signCollapseClassName,
+    filter_show_class_name: filterShowClassName,
+    sign_bar_class_name: signBarClassName,
+    vertical_bar_class_name: verticalBarClassName,
+    horizontal_bar_class_name: horizontalBarClassName,
+    rotating_sign_class_name: rotatingSignClassName,
+    hide_sign_class_name: hideSignClassName
+};
+
+// Show less or more class names
+const showMoreClassName = "show-more";
+const showLessOrMoreClassName = "show-less-or-more";
+const showLessClassName = "show-less";
+const filterShrinkClassName = "filter-shrink";
+
+const showLessOrMoreClassNames = {
+    show_more_class_name: showMoreClassName,
+    show_less_or_more_class_name: showLessOrMoreClassName,
+    show_less_class_name: showLessClassName,
+    filter_shrink_class_name: filterShrinkClassName
+};
+
+function composeClassName(...classNames)
+{
+    let classNameString = "";
+    classNames.forEach(cn => classNameString += cn + " ");
+
+    return classNameString;
+}
+
 function hideFilter(filterName)
 {
     let filterClassName = "filter_" + filterName;
@@ -8,18 +52,18 @@ function hideFilter(filterName)
 
     if (!filter) return;
 
-    if (filter.classList.contains("filter-collapse")) {
-        filter.classList.add("filter-show");
-        filter.classList.remove("filter-collapse");
+    if (filter.classList.contains(filterCollapseClassNames)) {
+        filter.classList.add(filterShowClassName);
+        filter.classList.remove(filterCollapseClassNames);
 
-        rotatingBar.classList.add("sign-open");
-        rotatingBar.classList.remove("sign-collapse");
+        rotatingBar.classList.add(signOpenClassName);
+        rotatingBar.classList.remove(signCollapseClassName);
     } else  {
-        filter.classList.remove("filter-show");
-        filter.classList.add("filter-collapse");
+        filter.classList.remove(filterShowClassName);
+        filter.classList.add(filterCollapseClassNames);
 
-        rotatingBar.classList.add("sign-collapse");
-        rotatingBar.classList.remove("sign-open");
+        rotatingBar.classList.add(signCollapseClassName);
+        rotatingBar.classList.remove(signOpenClassName);
     }
 }
 
@@ -30,19 +74,19 @@ function showMore(fieldName) {
     let opElement = document.querySelector(`.${opClassName}`);
     let filterCont = document.querySelector(`.${filterContClassName}`).firstChild;
 
-    if (opElement.classList.contains("show-more")) {
-        opElement.classList.remove("show-more");
-        opElement.classList.add("show-less");
+    if (opElement.classList.contains(showMoreClassName)) {
+        opElement.classList.remove(showMoreClassName);
+        opElement.classList.add(showLessClassName);
         opElement.innerHTML = "&uarr; show less";
 
-        filterCont.classList.remove("filter-shrink");
+        filterCont.classList.remove(filterShrinkClassName);
     } else {
-        opElement.classList.remove("show-less");
-        opElement.classList.add("show-more");
+        opElement.classList.remove(showLessClassName);
+        opElement.classList.add(showMoreClassName);
         opElement.innerHTML = "&darr; show more";
 
-        filterCont.classList.add("filter-shrink");
+        filterCont.classList.add(filterShrinkClassName);
     }
 }
 
-export { showMore, hideFilter };
+export { showMore, hideFilter, composeClassName, hideFilterClassNames, showLessOrMoreClassNames };
