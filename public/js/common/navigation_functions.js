965d996d894d9087434a26746a6b33fd89042315
Garnik Gasparyan <gasp.garnik@gmail.com>
2020-08-07 16:19:55 +0400

    make dropdown btn nioticable

--- public/js/common/navigation_functions.js
+++ public/js/common/navigation_functions.js
@@ -1,12 +1,17 @@
 function productsDropDown() {
     let dropDown = document.querySelector("#products-drop-down");
+    let arrow = document.querySelector(".drop-state-arr");
     if (!dropDown) return;
 
     if (dropDown.classList.contains("hide")) {
         dropDown.classList.remove("hide");
         dropDown.classList.add("show");
+
+        arrow.innerHTML = "&uarr;";
     } else {
         dropDown.classList.remove("show");
         dropDown.classList.add("hide");
+
+        arrow.innerHTML = "&darr;";
     }
 }
