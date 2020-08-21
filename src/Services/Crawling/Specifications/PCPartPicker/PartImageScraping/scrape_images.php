f66b0cbc56197837ad07b526fc2545e6960357e5
Garnik Gasparyan <gasp.garnik@gmail.com>
2020-08-21 17:03:49 +0400

    Crawl image if part don't have any

--- src/Services/Crawling/Specifications/PCPartPicker/PartImageScraping/scrape_images.php
+++ src/Services/Crawling/Specifications/PCPartPicker/PartImageScraping/scrape_images.php
@@ -17,22 +17,24 @@ use App\Database\Entities\
 use App\Services\Crawling\Specifications\PCPartPicker\PartImageScraping\ImageScraperFactory;
 
 $entitiesNames = [
-    PcCase::class,
-    PowerSupply::class,
-    VideoCard::class,
-    Cpu::class,
-    Cooler::class,
-    Storage::class,
-    Memory::class,
-    Motherboard::class
+    PcCase::class => "getCaseImages",
+    PowerSupply::class => "getPsuImages",
+    VideoCard::class => "getGpuImages",
+    Cpu::class => "getCpuImages",
+    Cooler::class => "getCoolerImages",
+    Storage::class => "getStorageImages",
+    Memory::class => "getMemoryImages",
+    Motherboard::class => "getMoboImages"
 ];
 
 $em = Connection::getEntityManager();
 
-foreach ($entitiesNames as $entityName) {
+foreach ($entitiesNames as $entityName => $mName) {
     $entities = $em->getRepository($entityName)->findAll();
     $scraper = ImageScraperFactory::create($entityName);
 
-    foreach ($entities as $entity)
-        $scraper->crawl($entity->getUrl(), $entity->getId());
+    foreach ($entities as $entity) {
+        if (!count($entity->$mName()))
+            $scraper->crawl($entity->getUrl(), $entity->getId());
+    }
 }
